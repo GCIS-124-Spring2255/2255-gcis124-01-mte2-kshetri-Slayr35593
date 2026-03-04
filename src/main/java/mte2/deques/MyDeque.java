ff078cafa828afe7ead6d52dfb9048e1b07861b8
Maelyn A. VonStettina-May (RIT Student) <pjv9265@rit.edu>
2026-03-04 11:20:16 -0500

    still working

--- src/main/java/mte2/deques/MyDeque.java
+++ src/main/java/mte2/deques/MyDeque.java
@@ -4,27 +4,51 @@
 
 package mte2.deques;
 
-// import java.util.LinkedList;
-// import java.util.Iterator;
+import java.util.Iterator;
 
-public class MyDeque {
-// public class MyDeque<E> implements Deque<E> {
+ import java.util.LinkedList;
+import java.util.Iterator;
+
+ public class MyDeque<E> implements Deque<E> {
     
-    // private final LinkedList<E> list;
-    // public MyDeque() {    list = new LinkedList<>();    }
+     private final LinkedList<E> list;
+     public MyDeque() {    
+        list = new LinkedList<>();    
+    }
 
     // ... for enqueueFront ...
-
+    @Override 
+    public void enqueueFront(E element) {
+        list.add(element);
+    }
     // ... for enqueueBack ...
-
+    @Override
+    public void enqueueBack(E element) {
+        list.addLast(element);
+    }
     // ... for dequeueFront ...
-
+    @Override
+    public E dequeueFront() {
+        list.removeFirst();
+        return list.removeFirst();
+    }
     // ... for dequeueBack ... 
-
+    @Override
+    public E dequeueBack() {
+        return list.removeLast();
+    }
     // ... for size ...
-
+    @Override
+    public int size() {
+        
+        return list.size();
+    }
     // ... for iterator ...
+    @Override
+    public Iterator<E> iterator() {
 
+        return null;
+    }
 
     public static void main(String[] args) { 
 
