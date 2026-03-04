123c1ab54b541e46794596062af7d8d63b80714c
Maelyn A. VonStettina-May (RIT Student) <pjv9265@rit.edu>
2026-03-04 10:50:03 -0500

    Shapes compleate

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -1,21 +1,35 @@
 // Question: Q1 (mte2, gcis124, 2255)
 // Package name: shapes
 // File name: Circle.java
-
 package mte2.shapes;
 
-public class Circle {
-// public class Circle implements Shape {
+ public class Circle implements Shape {
     
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
+     private final double radius;
+    
+     public Circle(double radius) {    this.radius = radius;    }
 
-    // @Override
-    // @Override
+     double pi = 3.141;
+       
+    
+
+     @Override
+     public double area(){
+            return pi * radius * radius;
+        }
+     @Override
+     public double perimeter(){
+        return 2 * pi * radius;
+
+    //@Override
+    //public String toString() {
+    //    return "" + radius + " " + perimeter;
+    //}
+     }
 
     public static void main(String[] args) {
-        
-        // ...
+        Circle c = new Circle(5);
+     System.out.println(c);
 
     }
 }
